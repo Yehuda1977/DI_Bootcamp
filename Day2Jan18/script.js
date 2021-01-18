93cc753cf97049e93b30b9c5c9e3f92c20c15067
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-01-18 14:57:17 +0200

    3rd week second day assignment

---
+++ Day2Jan18/script.js
@@ -0,0 +1,38 @@
+// Favorite Color
+
+let me = ["my","favorite","color","is","blue"]
+console.log(me.join(' '));
+
+// Mixup
+
+let str1 = 'mix'; 
+let str2 = 'pod'; 
+
+let newWord = (str2.slice(0,2) + str1.slice(-1)) + ' ' + (str1.slice(0,2) + str2.slice(-1))
+console.log(newWord)
+
+// Calculator
+
+let num1 = prompt('First number')
+let num2 = prompt('Second number')
+
+alert(Number(num1) + Number(num2))
+
+// Finding Nemo
+
+let string = 'I found Nemo at the order of the word you find!'
+let index = string.indexOf('Nemo');
+if(index != -1){
+    console.log(`I found Nemo at ${index}`)
+} else {
+    console.log('I did not find Nemo')
+}
+
+// XP Ninja
+// Exercise 3
+let addition = prompt('Enter two number seperated by a comma and a space')
+let additionArray = addition.split(', ');
+
+let sum = Number(additionArray[0]) + Number(additionArray[1]);
+alert(sum);
+
