57fd81f4d3687b09827abda99b52d4d8df8bdd48
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-02-14 19:16:58 +0200

    fixed caesar cypher

--- Week6Python/Day6Feb12/caesar.py
+++ Week6Python/Day6Feb12/caesar.py
@@ -1,8 +1,20 @@
 def caesar_cypher(text):
     cypher_text = ''
     for letter in text:
-        cypher_text += chr(ord(letter) + 3)
+        if letter == 'Z':
+            cypher_text += 'C'
+        elif letter == 'z':
+            cypher_text += 'c'
+        elif letter == ' ':
+            cypher_text += ' '
+        
+        else:
+            cypher_text += chr(ord(letter) + 3)
     return cypher_text
 
-text = input('Enter a text')
+
+
+text = input('Enter a text: ')
 print(caesar_cypher(text))
+
+
