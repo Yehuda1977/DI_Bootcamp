a5b394e3a7183eeea65def7ef462c6eaa0ef7f3a
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-02-23 18:25:09 +0200

    fixed something

--- Week8PythonOOP/Day1Feb21/Day2Feb22/dailychallenge.py
+++ Week8PythonOOP/Day1Feb21/Day2Feb22/dailychallenge.py
@@ -5,7 +5,15 @@ class Farm:
         self.name = name
         print(f'{name} Farm has been created')
     def get_info(self):
-        print('E-I-E-I-O!')
+        msg = ""
+        msg += f"{self.name}'s Farm" + "\n"
+        msg += "\n"
+
+        for animal, count in self.animals.items():
+            msg += f"{animal}: {count}" + "\n" 
+
+        msg += "\nE-I-E-I-O!"
+        return msg
     def add_animal(self, new_animal, number=1):
         if new_animal not in self.animals:
             self.animals.update({new_animal:number})
@@ -22,7 +30,6 @@ class Farm:
         for key in self.animals.keys():
             animal_types.append(key)
         animal_types.sort()
-        
         return animal_types
     def get_short_info(self):
         short_info = ('(s), ').join(self.get_animal_types())
@@ -31,13 +38,14 @@ class Farm:
         
 
 McDonalds = Farm('McDonalds')
-McDonalds.get_info()
+
 McDonalds.add_animal('Goat', 25)
 McDonalds.add_animal('Donkey')
 McDonalds.add_animal('Donkey')
 McDonalds.add_animal('Chicken', 14)
 McDonalds.add_animal('Horse')
 McDonalds.add_animal('Pig', 2)
+print(McDonalds.get_info())
 McDonalds.list_animals()
 print(McDonalds.get_animal_types())
 McDonalds.get_short_info()
